2c402110ef3b5d03e491207ee6a33e4ff8b38fdc
HanSolCho <whgksthf702@naver.com>
2020-07-30 15:33:27 +0900

    Update pluscycle.java

--- algorithm/src/pluscycle.java
+++ algorithm/src/pluscycle.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+// 주석을 안달아놔서 어디서 푼 문제인지는 모름
+// a값을 특정 규칙으로 나눠서 나온 나머지를 더하는 방식으로 아마 나머지+십의자리수 이런식이었던거 같음
+// 그 값이 다시  a값과 동일할때까지 카운트 세는것
 public class pluscycle {
 	public static void main(String[] args) {
 		int count;
